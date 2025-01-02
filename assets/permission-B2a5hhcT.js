7abafadf0db051e379f9ff12db127c12c0af5f13
jiaoy <18898159853@163.com>
2025-01-02 11:25:15 +0800

    1

--- assets/permission-B2a5hhcT.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-Cu8e7n-j.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/permission-nYEC8bYp.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-CApY-42s.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
