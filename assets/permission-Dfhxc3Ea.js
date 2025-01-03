c1e813d4c8a0928c113892dfb51699e974198a5a
jiaoy <18898159853@163.com>
2025-01-03 15:57:01 +0800

    1

--- assets/permission-Dfhxc3Ea.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-DlLV7eJe.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/permission-Dn1j-x2l.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-Ddi2sB0k.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
