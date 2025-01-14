a90bb663fd4ddf720e19a3a8752858b3dd55392c
jiaoy <18898159853@163.com>
2025-01-14 11:24:56 +0800

    1

--- assets/permission-ffzZUtKl.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-CLDogeON.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/round-plus-DB21dsS3.js
@@ -0,0 +1,1 @@
+import{o as e,e as o,f as n}from"./index-W31l-hMM.js";const t={class:"inline-block",viewBox:"0 0 24 24",width:"1em",height:"1em"},c=n("path",{fill:"currentColor",d:"M18 12.998h-5v5a1 1 0 0 1-2 0v-5H6a1 1 0 0 1 0-2h5v-5a1 1 0 0 1 2 0v5h5a1 1 0 0 1 0 2"},null,-1),s=[c];function a(i,l){return e(),o("svg",t,[...s])}const _={name:"ic-round-plus",render:a};export{_};
