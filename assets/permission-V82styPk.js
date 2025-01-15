c13dfa206031104079b21e2a618f2322da3d2ebe
jiaoy <18898159853@163.com>
2025-01-15 16:09:23 +0800

    1

---
+++ assets/permission-V82styPk.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-B_SnzBFt.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
--- assets/permission-argpRA4v.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-g00S9pbc.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
