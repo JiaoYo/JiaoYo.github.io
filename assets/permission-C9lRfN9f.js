764cdd46213d39ef4b58b92515a834bc7511b8fe
jiaoy <18898159853@163.com>
2025-01-18 15:44:13 +0800

    1

--- assets/permission-C9lRfN9f.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-B6hlJdrz.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/permission-DtCh6dEo.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-D9LKuiQm.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
