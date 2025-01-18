844aaa5068bb2611b7f8be74244923687add4e01
jiaoy <18898159853@163.com>
2025-01-18 14:18:59 +0800

    1

--- assets/permission-C6Vah77e.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-DU0tn_QU.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/permission-C9lRfN9f.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-B6hlJdrz.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
