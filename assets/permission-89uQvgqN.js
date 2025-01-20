c3b63242382eb4670c609fa6bda9afbe045261af
jiaoy <18898159853@163.com>
2025-01-20 09:23:54 +0800

    '1'

--- assets/permission-89uQvgqN.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-DO9pjeOx.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/permission-DhdEmILJ.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-D8Ssns-q.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
