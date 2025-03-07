77e2ebd2d526315c968a99b90dfad6b626648f95
jiaoy <18898159853@163.com>
2025-03-07 09:30:08 +0800

    1

---
+++ assets/permission-Bx9DMYgP.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-DUtpuk6Y.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
---
+++ assets/task-DA5gOtRn.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-DUtpuk6Y.js";function r(t){return e({url:"/metar/info.do",method:"post",params:t})}export{r as f};
