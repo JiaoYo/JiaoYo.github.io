5042ba925e83df27ff03864495c6048af9baf75f
jiaoy <18898159853@163.com>
2025-03-09 19:39:21 +0800

    1

---
+++ assets/data_report-BJ48sRnC.js
@@ -0,0 +1,1 @@
+import{aq as o}from"./index-CQx2Ff8S.js";function e(t){return o({url:"/report/info.do",method:"post",params:t})}export{e as f};
---
+++ assets/permission-BwrcNMjh.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-CQx2Ff8S.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
