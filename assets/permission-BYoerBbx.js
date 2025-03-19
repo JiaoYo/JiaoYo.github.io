d6a7af7a42ff332710507599342579776e1646fb
jiaoy <18898159853@163.com>
2025-03-19 14:51:50 +0800

    1

---
+++ assets/permission-BYoerBbx.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-7Q9xZuwa.js";const r=e(),u=s=>r.userInfo.permissions.some(o=>o==s);export{u as h};
--- assets/task-CkEJtohk.js
+++
@@ -1,1 +0,0 @@
-import{at as e}from"./index-BYyX0kxW.js";function r(t){return e({url:"/metar/info.do",method:"post",params:t})}export{r as f};
