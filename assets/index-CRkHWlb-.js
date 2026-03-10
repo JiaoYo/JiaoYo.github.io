4e7d11fef9fcb61076053389d150923fe95c7ebf
jiaoy <18898159853@163.com>
2026-03-10 16:35:53 +0800

    1

---
+++ assets/index-CRkHWlb-.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./exception-base.vue_vue_type_script_setup_true_lang-C8o9TCxH.js";import{d as n,c as t,o as a}from"./index-DcM_B_E8.js";const m=n({name:"403",__name:"index",setup(c){return(_,s)=>{const e=o;return a(),t(e,{type:"403"})}}});export{m as default};
---
+++ assets/permission-iV8I6bEw.js
@@ -0,0 +1,1 @@
+import{z as r}from"./index-DcM_B_E8.js";const t=r(),h=o=>{var s;return(s=t.userInfo.permissions)==null?void 0:s.some(e=>e==o)};export{h};
