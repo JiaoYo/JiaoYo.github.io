4122a9bbb59579e9165ec7fd81f67d5daa30f823
jiaoy <18898159853@163.com>
2026-05-07 09:39:32 +0800

    1

---
+++ assets/auth-mrj2FpPg.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-x36q2_JF.js";function o(){const t=e();function n(u){return t.isLogin?typeof u=="string"?t.userInfo.buttons.includes(u):u.some(r=>t.userInfo.buttons.includes(r)):!1}return{hasAuth:n}}export{o as u};
