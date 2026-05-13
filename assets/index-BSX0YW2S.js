f5044dc7eddd8269616800fc72440ad2b2ec50ad
jiaoy <18898159853@163.com>
2026-05-13 10:01:14 +0800

    1

---
+++ assets/index-BSX0YW2S.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./exception-base.vue_vue_type_script_setup_true_lang-DUXGE93E.js";import{d as n,c as t,o as a}from"./index-CWp-Lr6x.js";const m=n({name:"403",__name:"index",setup(c){return(_,s)=>{const e=o;return a(),t(e,{type:"403"})}}});export{m as default};
