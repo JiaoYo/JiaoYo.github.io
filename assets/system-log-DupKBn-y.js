d35535fbfdbfe7dd311fcb7e9dfe59934bc10cdf
jiaoy <18898159853@163.com>
2026-05-21 13:57:14 +0800

    '1'

--- assets/system-log-DupKBn-y.js
+++
@@ -1,1 +0,0 @@
-import{b4 as e}from"./index-Bt-7J3R6.js";function r(t){return e({url:"/sys/log/login/page",method:"get",params:t})}function g(t){return e({url:"/sys/log/operation/page",method:"get",params:t})}function s(t){return e({url:"/sys/log/error/page",method:"get",params:t})}export{r as a,g as b,s as f};
